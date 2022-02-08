78fe05323967b4f9e767e5d43d2037eb2db1b841
mukesh kumar <msingh.mukesh@gmail.com>
2022-02-08 11:46:46 +0100

    TaxiFareModelAdvance checking

---
+++ TaxiFareModelAdvanced/params.py
@@ -0,0 +1,38 @@
+
+### GCP configuration - - - - - - - - - - - - - - - - - - -
+
+# /!\ you should fill these according to your account
+
+### GCP Project - - - - - - - - - - - - - - - - - - - - - -
+
+# not required here
+
+### GCP Storage - - - - - - - - - - - - - - - - - - - - - -
+
+
+PROJECT_ID = 'mukeshlawagonproject'
+BUCKET_NAME = 'wagon-data-739-m4msingh'
+##### Data  - - - - - - - - - - - - - - - - - - - - - - - -
+
+# train data file location
+# /!\ here you need to decide if you are going to train using the provided and uploaded data/train_1k.csv sample file
+# or if you want to use the full dataset (you need need to upload it first of course)
+BUCKET_TRAIN_DATA_PATH = 'data/train_1k.csv'
+
+##### Training  - - - - - - - - - - - - - - - - - - - - - -
+
+# not required here
+
+##### Model - - - - - - - - - - - - - - - - - - - - - - - -
+
+# model folder name (will contain the folders for all trained model versions)
+MODEL_NAME = 'taxifareadvance'
+
+# model version folder name (where the trained model.joblib file will be stored)
+MODEL_VERSION = 'Pipeline'
+
+### GCP AI Platform - - - - - - - - - - - - - - - - - - - -
+
+# not required here
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - -
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='TaxiFareModelAdvanced',
+      version='1.0',
+      install_requires=requirements,
+      packages=find_packages(),
+      include_package_data=True,
+      description='Taxi Fare Prediction Pipeline')
